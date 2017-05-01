f5b738956835f434beb15db65b5b54147b4a24a8
Brian Du <brian.bh.du@gmail.com>
2017-04-30 22:54:20 -0400

    Remove input error when hovering over input

--- js/main.js
+++ js/main.js
@@ -9,3 +9,11 @@ function closeModal() {
 function error() {
   $('input').css('outline','2px solid #bc2a39');
 }
+
+function mouseoverUser() {
+  $('input[type=text]').css('outline','none');
+}
+
+function mouseoverPass() {
+  $('input[type=password]').css('outline','none');
+}
